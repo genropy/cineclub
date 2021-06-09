941517f17f5fe8788d26fccf1d9a54dfba40afe1
Saverio Porcari <>
2021-06-09 16:17:58 +0200

    no message

--- packages/cine/menu.py
+++ packages/cine/menu.py
@@ -4,8 +4,8 @@ def config(root,application=None):
     auto = root.branch(u"Cineclub")
     auto.thpage(u"Soci", table="cine.socio")
     auto.thpage(u"Proiezioni", table="cine.proiezione")
-    auto.thpage(u"Recensioni", table="cine.recensione")
-    auto.thpage(u"Iscrizioni", table="cine.iscrizione_proiezione")
+    #auto.thpage(u"Recensioni", table="cine.recensione")
+    #auto.thpage(u"Iscrizioni", table="cine.iscrizione_proiezione")
     auto.thpage(u"Film", table="cine.film")
     auto.lookups('Tabelle lookup', lookup_manager='cine')
 
